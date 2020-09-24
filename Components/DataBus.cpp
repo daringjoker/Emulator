f8079238504251737fae68794bedc971815adf07
uniqueram <uniqueram4@gmail.com>
2020-09-24 20:20:53 +0545

    added fuction.

--- Components/DataBus.cpp
+++ Components/DataBus.cpp
@@ -1,5 +1,26 @@
 //
 // Created by DaringJoker on 9/23/2020.
-//
 
 #include "DataBus.h"
+bool datavalidity(string a)
+{
+    int l=a.length();
+    if(l==2)
+    {
+        if((a[0]>='0'&&a[0]<='9')||(a[0]>='A'&&a[0]<='F'))
+        {
+            if((a[1]>='0'&&a[1]<='9')||(a[1]>='A'&&a[1]<='F'))
+                return true;
+            else
+                return false;
+        }
+        else
+            return false;
+    }
+    else
+        return false;
+}
+
+bool DataBus::datavalidity() {
+    return false;
+}
--- Components/DataBus.h
+++ Components/DataBus.h
@@ -1,12 +1,14 @@
 //
 // Created by DaringJoker on 9/23/2020.
-//
+//created by uniqueram on 9/24/2020
 
 #ifndef EMULATOR_DATABUS_H
 #define EMULATOR_DATABUS_H
 
 
 class DataBus {
+public:
+    bool datavalidity();
 
 };
 
