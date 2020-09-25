8ff3a24d27e125e4ea8bbff21e5ca8540fcdffc7
Sulochan2009 <zakiraasin2009@gmail.com>
2020-09-25 14:41:23 +0545

    mem header

--- Components/Memory.h
+++ Components/Memory.h
@@ -4,10 +4,18 @@
 
 #ifndef EMULATOR_MEMORY_H
 #define EMULATOR_MEMORY_H
+#include "AddressBus.h"
+#include "DataBus.h"
+#include "constants.h"
 
 
 class Memory {
+private:
 
+public:
+    Memory(AddressBus,DataBus);
+void ReadMemory();
+void WriteMemory();
 
 };
 
